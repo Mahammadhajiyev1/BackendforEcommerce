562b6c04c21dcd5e66caf045fb3f269ba8282762
Mahammad Hajiyev <MahammadHajiyev1@gmail.com>
2022-04-07 14:28:17 +0400

    upload

---
+++ routes/orders.js
@@ -0,0 +1,180 @@
+const { Order } = require("../models/order");
+const express = require("express");
+const { OrderItem } = require("../models/order-item");
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
+
+  if (!orderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(orderList);
+});
+
+router.get(`/:id`, async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    });
+
+  if (!order) {
+    res.status(500).json({ success: false });
+  }
+  res.send(order);
+});
+
+router.post("/", async (req, res) => {
+  const orderItemIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+
+      newOrderItem = await newOrderItem.save();
+
+      return newOrderItem._id;
+    })
+  );
+
+  const orderItemsIdsResolved = await orderItemIds;
+
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
+  let order = new Order({
+    orderItems: orderItemsIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: totalPrice,
+    user: req.body.user,
+  });
+  order = await order.save();
+
+  if (!order) return res.status(400).send("The order can not be created!");
+
+  res.send(order);
+});
+
+router.put("/:id", async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!order) return res.status(400).send("The order can not be updated!");
+
+  res.send(order);
+});
+
+router.delete("/:id", (req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res
+          .status(200)
+          .json({ success: true, message: "The order is deleted" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "Order not found!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
+router.get("/get/totalsales", async (req, res) => {
+  const totalSales = await Order.aggregate([
+    { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
+  ]);
+
+  if (!totalSales) {
+    return res.status(400).send("The order sales can not be generated");
+  }
+
+  res.send({ totalSales: totalSales.pop().totalsales });
+});
+
+router.get(`/get/count`, async (req, res) => {
+  const orderCount = await Order.countDocuments((count) => count);
+  if (!orderCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({ orderCount: orderCount });
+});
+
+router.get(`/get/userorders/:userid`, async (req, res) => {
+  const userOrderList = await Order.find({ user: req.params.userid })
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    })
+    .sort({ dateOrdered: -1 });
+
+  if (!userOrderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userOrderList);
+});
+
+module.exports = router;
+
+/*
+{
+    "orderItems":[
+        {
+            "quantity":3,
+            "product" : ""
+            
+        },
+        {
+            "quantity": 2,
+            "product" : ""
+        }
+    ],
+    "shippingAddress1": "Flowers Street, 45",
+    "shippingAddress2" : "1-B",
+    "city" : "Parague",
+    "zip": "00000",
+    "country": "Czech Republic",
+    "phone":"+8398174912",
+    "user":""
+
+    
+
+
+
+}
+
+*/
---
+++ routes/users.js
@@ -0,0 +1,114 @@
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.get(`/`, async (req, res) => {
+  const userList = await User.find().select("-passwordHash");
+
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+  if (!user) {
+    res.status(500).json({
+      message: "The user with the given id was not found",
+    });
+  }
+  res.status(200).send(user);
+});
+
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    street: req.body.street,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) return res.status(400).send("The user can not be registered!");
+
+  res.send(user);
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  const secret = process.env.secret;
+
+  if (!user) return res.status(400).send("The user can not be found");
+
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        isAdmin: user.isAdmin,
+      },
+      secret,
+      { expiresIn: "1d" }
+    );
+    res.status(200).send({ user: user.email, token: token });
+  } else {
+    res.status(400).send("Password is wrong");
+  }
+});
+
+router.post("/register", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    street: req.body.street,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) return res.status(400).send("The user can not be registered!");
+
+  res.send(user);
+});
+
+router.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res
+          .status(200)
+          .json({ success: true, message: "The user is deleted" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
+router.get(`/get/count`, async (req, res) => {
+  const userCount = await User.countDocuments((count) => count);
+  if (!userCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({ userCount: userCount });
+});
+
+module.exports = router;
